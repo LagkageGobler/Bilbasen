f0f03cd4d9c1e281aaa5ce1f57931a2a8111e69a
simen <simenhenrikaen@gmail.com>
2026-03-10 11:30:20 +0100

    lavede virtuel envirement og begyndt at lave tables

--- Main.py
+++ Main.py
@@ -6,3 +6,21 @@ def StartDatabase(db_name):
         return sqlite3.connect(db_name)
     except Exception as e:
         print(e)
+        raise
+
+# lav table
+def DBtable(connection):
+    query = """
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY
+        name TEXT
+        emil TEXT UNIQUE
+        password TEXT
+    )
+    """
+    try:
+        with connection:
+            connection.execute(query)
+    except Exception as e:
+        print(e)
+        raise
