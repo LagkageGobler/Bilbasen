f7d609016efe3f987e273a6a6e06adceda63d7e7
filip <filipskydt@gmail.com>
2026-03-23 12:14:04 +0100

    ferdig med at scrape links fra bilbasen

--- test-scrape.py
+++ test-scrape.py
@@ -1,7 +1,19 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.bilbasen.dk/brugt/bil?includeengroscvr=true&includeleasing=false&page=2"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html")
-print(soup.find("article"))
+url = "https://www.bilbasen.dk/brugt/bil?includeengroscvr=true&includeleasing=false"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+selector = "#__next > div.nmp-ads-layout__wrapper > div.nmp-ads-layout__page > div.nmp-ads-layout__content > div > main > div.Pagination_pagination__GywrN > div > span:nth-child(2)"
+
+element = soup.select_one(selector)
+
+print(element.text)
