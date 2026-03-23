bb00b527180fd11220ebd60be98690c5c62ec6a0
simen <simenhenrikaen@gmail.com>
2026-03-23 10:16:58 +0100

    menu

--- Main.py
+++ Main.py
@@ -6,3 +6,29 @@ while True:
     print("4. Sorter biler ud fra Mærke til pris")
     print("5. Update database(Tager mega lang tid)")
     print("6. Quit")
+
+    valg = input("Choose (1-6): ")
+
+    if valg == "1":
+        pass
+
+    elif valg == "2":
+        pass
+
+    elif valg == "3":
+        pass
+
+    elif valg == "4":
+        pass
+
+    elif valg == "5":
+        pass
+
+    elif valg == "6":
+        print("Bye, bye")
+        break
+    
+    else:
+        print("Bevar dit svar inde for rangen vi har angivet tak")
+
+    
