ec188b9fe8ebc825a36c02e9efab67961dfc0231
simen <simenhenrikaen@gmail.com>
2026-03-23 09:18:03 +0100

    laver på menu og rykket database func ind i egen file

--- Main.py
+++ Main.py
@@ -1,47 +1,10 @@
-#imports
-import sqlite3 
-
-#Script wide variables
-db_name = "bilgovrum.db"
-# setup database
-def StartDatabase(db_name):
-    try:
-        return sqlite3.connect(db_name)
-    except Exception as e:
-        print(e)
-        raise
-
-# lav table
-def DBtable(db_name):
-    conn = sqlite3.connect(db_name)
-    cur = conn.cursor()
-    try:
-        cur.execute("create table if not exists users(name, emil, password)")
-        cur.execute("create table if not exists biler(id, prisPrKm, modelÅr, drivemidel, BilNavn, Url)")
-    except Exception as e:
-        print(e)
-        raise
-    conn.commit()
-    conn.close()
-#slet datbase
-def Update(db_name):
-    sql = "DELETE FROM biler WHERE id = ?"
-    conn = sqlite3.connect(db_name)
-    cur = conn.cursor()
-    for i in sql.count(id):
-        cur.execute(sql, i)
-        i += 1
-    conn.commit()
-    conn.close()
-#add users to database
-def ADDuser(db_name, UserName, Email, Password):
-    conn = sqlite3.connect(db_name)
-    cur = conn.cursor()
-    cur.execute("insert into users values (?, ?, ?)", (UserName, Email, Password))
-    conn.commit()
-    conn.close()
-#main
-def main():
-    StartDatabase(db_name)
-    DBtable(db_name)
-main()
+while True:
+    print("\n--- MENU ---")
+    print("1. ")
+    print("2. ")
+    print("3. ")
+    print("4. ")
+    print("5. ")
+    print("6. ")
+    print("7. ")
+    print("8. Quit")
